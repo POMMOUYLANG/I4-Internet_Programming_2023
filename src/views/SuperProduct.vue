ad58df493218ba23913880ece95a787bbf047ac5
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-11-07 12:22:57 +0700

    TP01

--- src/views/SuperProduct.vue
+++ src/views/SuperProduct.vue
@@ -38,7 +38,7 @@ export default {
     return {
         Categories: [
         {
-          Img: "https://s3-alpha-sig.figma.com/img/8370/e915/8a586f746c0b10b70cd405c8aa1f2038?Expires=1699228800&Signature=L-reokY4cdElwI8HHeS9iKW58ip98C93-H~HpTII~Px9MXnLDATcnx-ZaQv5AXB6weQwhTXCekkyWSSLxJGsBqOLWxlzhDKADaXO-4HoKz6q-crjH6nX6-XIW0HQzfmxFRIGb9NP6sxwik7gx5fDpOzQLeVu5U~Js6kcysvwNVBRf9g90BJBjOWxV6UyIVchOYCaRFZgvZv-pCGevQhQ0RzENRzyWu15Ps~xweWRVAFAqYQwSO~hQbFULB~4ohbZ9Zxu3sxuXEyWg8Tg8P3KWIzvM2J190-H2Gazbsy-scpr4-rsf9PXWPMCD~i62IEhAAo8xjJwj5yyDR-a-VY8LQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+          Img: "https://s3-alpha-sig.figma.com/img/8370/e915/8a586f746c0b10b70cd405c8aa1f2038?Expires=1700438400&Signature=G~VHy~MbjQA8yedex0XzmXgIdiMaIoh1kRKDI8oc4FallqvnC51fWPXbng6LNFKpU34yZPXgEJTIEEg-TyOQ0V6CB7~gCE~cQubmlmB9Sok2wuuXFn2Z4iWIJ8jYMJFZeOkwABbT-XbfOUcyq8vly4QdaYHdg1bkzGl6qtchkCh~G~RNzEdeOqJoCifuuBPQfqHrQ9sosz33SYiiooKAcL~vorsGV0PXDE5un0rMwi5Le9XPL88Q~Vs4ql9adKPqZfYFeXmTktUxkwSmh08CGA0xwwUX5NM~ulAf3y4e4FAkzCuy3PWDgQUCUxpmRKUrDtgrHEqk40eDlRoOMUS-ng__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
           bg: "rgba(242, 253, 228, 1)",
           ItemName: "Burger",
           ItemNumber: "14 Items",

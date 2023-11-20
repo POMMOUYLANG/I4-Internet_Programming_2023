bd58a30d81ef07589448e737f01f24eb1b611bc2
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-11-20 21:40:54 +0700

    TP02

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Internet_Programming-I_2023/",
   plugins: [vue()],
   resolve: {
     alias: {

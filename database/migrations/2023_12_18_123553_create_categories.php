1f3e369136f436f2f504bacbd578a5850a1da8bd
POM-MOUYLANG <126035070+POMMOUYLANG@users.noreply.github.com>
2023-12-20 14:17:41 +0700

    TP04

--- database/migrations/2023_12_18_123553_create_categories.php
+++ database/migrations/2023_12_18_123553_create_categories.php
@@ -17,10 +17,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('categories');
---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['id' => 1, 'name' => 'Category A'],
+            ['id' => 2, 'name' => 'Category B'],
+            ['id' => 3, 'name' => 'Category C'],
+        ]);
+    }
+}

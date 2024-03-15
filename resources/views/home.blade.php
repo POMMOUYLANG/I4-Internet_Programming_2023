91ffa15d25abdca985490c96de39ba60e776fe05
POMMOUYLANG <mouylangpom@gamail.com>
2024-03-15 16:04:28 +0700

    TP07

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/app.css"/>
+    <title>{{ $title }}</title>
+</head>
+<body>
+    
+
+    <main>
+
+
+    </main>
+</body>
+</html>
+
+<style>
+    h1{
+        font-size: 1.4rem;
+    }
+    :root{
+        font-size: 16px;
+        background-color: white;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    body{
+        margin: auto;
+        padding:0;
+        width: fit-content;
+    }
+    .flex{
+        display:flex;
+    }
+    nav{
+        width: 100%;
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        justify-content: space-between;
+    }
+    main{
+        display:flex;
+        gap:4px;
+    }
+</style>
--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user',function(Request $request){
-    return $request->use();
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });
 
-Route::middleware('auth:api')->get('/test',function(){
+Route::middleware('auth:api')->get('/test', function () {
     return "Test page";
 });
 
